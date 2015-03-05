af5c9053f0dccc364fc2b80be627706b60e9039f
Nick Geoghegan <nick@mongodb.com>
2015-03-05 18:33:08 +0000

    Changes coding style

--- app/src/main/java/net/nickgeoghegan/scootybus/MainActivity.java
+++ app/src/main/java/net/nickgeoghegan/scootybus/MainActivity.java
@@ -12,9 +12,8 @@ import android.util.Log;
 import android.view.View;
 
 
-
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
 
     /**
      * Intent Request Codes
@@ -35,9 +34,9 @@ public class MainActivity extends ActionBarActivity {
     private BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -49,7 +48,8 @@ public class MainActivity extends ActionBarActivity {
         /**
          * Sanity check to ensure the device actually has a bluetooth adapter
          */
-        if (mBluetoothAdapter == null) {
+        if (mBluetoothAdapter == null)
+        {
             Toast.makeText(getApplicationContext(), "This device does not support bluetooth", Toast.LENGTH_LONG).show();
             Log.d(TAG, "This device does not support bluetooth... Requesting app to be killed");
 
@@ -60,49 +60,56 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    public void onButtonBluetoothConnect(View view) {
+    public void onButtonBluetoothConnect(View view)
+    {
 
         Log.d(TAG, "In onButtonBluetoothConnect()");
 
         /**
          * Enables the bluetooth adapter, if it's disabled, and sets the device into discovery mode
          */
-         if (!mBluetoothAdapter.isEnabled()) {
-             Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-             startActivityForResult(enableBlueTooth, REQUEST_ENABLE_BLUETOOTH);
-             Toast.makeText(getApplicationContext(), "Enabling Bluetooth", Toast.LENGTH_LONG).show();
-             Log.d(TAG, "Enabling Bluetooth");
-         }
-         else {
-             Toast.makeText(getApplicationContext(), "Bluetooth is already enabled", Toast.LENGTH_LONG).show();
-             Log.d(TAG, "Bluetooth is already enabled");
-         }
+        if (!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBlueTooth, REQUEST_ENABLE_BLUETOOTH);
+            Toast.makeText(getApplicationContext(), "Enabling Bluetooth", Toast.LENGTH_LONG).show();
+            Log.d(TAG, "Enabling Bluetooth");
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Bluetooth is already enabled", Toast.LENGTH_LONG).show();
+            Log.d(TAG, "Bluetooth is already enabled");
+        }
 
         /**
          * Enables bluetooth discovery, if it's not already enabled.
          * TODO: Verify that this is actually needed in real life if we have already paired with a device
          */
-        if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+        if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE)
+        {
             Intent enableDiscovery = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
             startActivityForResult(enableDiscovery, REQUEST_ENABLE_DISCOVERABILITY);
             Toast.makeText(getApplicationContext(), "Enabling Discovery", Toast.LENGTH_LONG).show();
             Log.d(TAG, "Enabling Discovery");
         }
-        else {
+        else
+        {
             Toast.makeText(getApplicationContext(), "Discovery is already enabled", Toast.LENGTH_LONG).show();
             Log.d(TAG, "Discovery is already enabled");
         }
 
     }
 
-    public void onButtonCloseApp(View view) {
+    public void onButtonCloseApp(View view)
+    {
 
         Log.d(TAG, "In onButtonCloseApp()");
 
         /**
          * Disables bluetooth
          */
-        if (mBluetoothAdapter.isEnabled()) {
+        if (mBluetoothAdapter.isEnabled())
+        {
             mBluetoothAdapter.disable();
             Toast.makeText(getApplicationContext(), "Disabling Bluetooth. Press again to close", Toast.LENGTH_LONG).show();
             Log.d(TAG, "Disabling Bluetooth");
@@ -110,7 +117,8 @@ public class MainActivity extends ActionBarActivity {
         /**
          * Actually closes the app
          */
-        else {
+        else
+        {
             Log.d(TAG, "Closing App");
             finish();
         }
@@ -118,21 +126,24 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 

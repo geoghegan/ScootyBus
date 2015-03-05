e9d7b634f3a5ac6991cd1dd063fd776a14b897c0
Nick Geoghegan <nick@mongodb.com>
2015-03-05 19:27:04 +0000

    Shows a list of paired devices

--- app/src/main/java/net/nickgeoghegan/scootybus/MainActivity.java
+++ app/src/main/java/net/nickgeoghegan/scootybus/MainActivity.java
@@ -1,15 +1,25 @@
 package net.nickgeoghegan.scootybus;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 import android.util.Log;
 import android.view.View;
+import android.app.ListActivity;
+import android.widget.ListView;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 
 public class MainActivity extends ActionBarActivity
@@ -32,6 +42,16 @@ public class MainActivity extends ActionBarActivity
      * Gets the device's default Bluetooth Adapter
      */
     private BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    /**
+     * A set of bonded / already paired devices
+     */
+    private Set<BluetoothDevice> pairedDevices;
+
+    /**
+     * A generic listView
+     */
+    private ListView listView;
 
 
     @Override
@@ -60,6 +80,10 @@ public class MainActivity extends ActionBarActivity
         }
     }
 
+    /**
+     * TODO: Remove the connection button altogether and init the BT interface on startup if it's not already enabled.
+     */
+
     public void onButtonBluetoothConnect(View view)
     {
 
@@ -74,6 +98,7 @@ public class MainActivity extends ActionBarActivity
             startActivityForResult(enableBlueTooth, REQUEST_ENABLE_BLUETOOTH);
             Toast.makeText(getApplicationContext(), "Enabling Bluetooth", Toast.LENGTH_LONG).show();
             Log.d(TAG, "Enabling Bluetooth");
+
         }
         else
         {
@@ -81,10 +106,10 @@ public class MainActivity extends ActionBarActivity
             Log.d(TAG, "Bluetooth is already enabled");
         }
 
-        /**
+       /* *//**
          * Enables bluetooth discovery, if it's not already enabled.
          * TODO: Verify that this is actually needed in real life if we have already paired with a device
-         */
+         *//*
         if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE)
         {
             Intent enableDiscovery = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
@@ -96,7 +121,7 @@ public class MainActivity extends ActionBarActivity
         {
             Toast.makeText(getApplicationContext(), "Discovery is already enabled", Toast.LENGTH_LONG).show();
             Log.d(TAG, "Discovery is already enabled");
-        }
+        }*/
 
     }
 
@@ -121,6 +146,42 @@ public class MainActivity extends ActionBarActivity
         {
             Log.d(TAG, "Closing App");
             finish();
+        }
+
+    }
+
+    /**
+     *  Show a list of paired devices
+     *  TODO: Get rid of the button and just show a clickable list on startup
+     */
+    public void showDevices(View view)
+    {
+
+        pairedDevices = mBluetoothAdapter.getBondedDevices();
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayList list = new ArrayList();
+
+        for (BluetoothDevice device : pairedDevices)
+        {
+            list.add(device.getName() + "\n" + device.getAddress());
+
+        }
+
+        listView = (ListView) findViewById(R.id.listView);
+        final ArrayAdapter adapter = new ArrayAdapter (this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        if(list.isEmpty() && mBluetoothAdapter.isEnabled())
+        {
+            Toast.makeText(getApplicationContext(), "No Paired Devices", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "No Paired Devices");
+
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Showing Paired Devices", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "Showing Paired Devices");
+
         }
 
     }
